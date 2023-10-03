4d95bacbd732359d314bc79d718557e29513e283
Balaguru <balagurusiva2002@gmail.com>
2023-10-03 13:26:02 +0530

    modified:   controller/loginController.js 	modified:   routes/login.js

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,4 +1,18 @@
-export const  loginValidator = (req,res,next) =>{
-  
+import { UsersId } from "../models/userIdModel.js"
+
+
+export const loginValidator = async (req, res, next) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    const result = await UsersId.findOne({$and : [{ email: email }, {password:password}]})
+ 
+             if(!result){
+                res.json({
+                    "error":"invalid data"
+                })
+             }else{
+                next()
+             }
 
 }
--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import { loginValidator } from '../controller/loginController.js'
+import { registerValidator } from '../controller/registerControl.js'
 
 const routes = express.Router()
 
-routes.post('/', loginValidator, (req,res) =>{
+routes.post('/',loginValidator, (req,res) =>{
     res.status(200).json({
         "message":'succesfully logged in and welcome bak',
     })
