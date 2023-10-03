04fa951f169dccbcde9dedf4ffd48f0e2c01a9c8
Balaguru <balagurusiva2002@gmail.com>
2023-10-03 14:47:02 +0530

    modified:   .gitignore 	deleted:    config.js 	modified:   index.js 	modified:   package-lock.json 	modified:   package.json

--- config.js
+++
@@ -1,3 +0,0 @@
-export const PORT =5000
-
-export const mongodbURL = 'mongodb://localhost:27017/login-page'
--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 import express from 'express'
-import bodyParser from 'body-parser'
-import { PORT, mongodbURL } from './config.js'
+import bodyParser from 'body-parser' 
 import regRoutes from './routes/register.js'
 import loginRoutes from './routes/login.js'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
+dotenv.config();
 const app = express()
 
 app.use(bodyParser.json())
 app.use('/register', regRoutes)
 app.use('/login', loginRoutes)
  
-
+console.log(process.env.envmongodbURL)
 
 mongoose
-    .connect(mongodbURL)
+    .connect(process.env.mongodbURL)
     .then(() => {
         console.log('mongo connected')
-        app.listen(PORT, () => { console.log("listending in port number 5000") })
+        app.listen(process.env.PORT, () => { console.log("listending in port number 5000") })
     })
     .catch((err) => {
         console.log(err)
